2ff74de4595672d280fab4657fb97877248bc7f3
Pei WenQian <pwq1989@163.com>
2013-08-27 17:54:32 +0800

    add email classes

---
+++ EmailSender.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using Dimac.JMail;
+namespace Helper
+{
+    /// <summary>
+    /// Smtp 的摘要说明
+    /// </summary>
+    public sealed class EmailSender
+    {
+        static Conf webconf = Conf.CreateFromWebConfig();
+        /// <summary>
+        /// cannot create an instance of this class
+        /// </summary>
+        private EmailSender() { }
+
+        /// <summary>
+        /// 发送Email到多个接受人
+        /// </summary>
+        /// <param name="strSubject"></param>
+        /// <param name="strContent"></param>
+        /// <param name="strFrom"></param>
+        /// <param name="listReceivers"></param>
+        /// <returns></returns>
+        public static void SendEmail(string strSubject, string strContent, List<string> listReceivers)
+        {
+            Message message = new Message();
+            message.From.Email = webconf.get("smtp.site.email", "busy_1@163.com") as string;
+            message.From.FullName = webconf.get("site.name", "武汉理工大学经纬网") as string;
+            foreach (string rcv in listReceivers)
+                message.To.Add(rcv);
+
+            message.Subject = strSubject;
+            message.Charset = System.Text.Encoding.UTF8;
+            message.BodyHtml = strContent;
+
+            string _server = webconf.get("smtp.server", "wutnews.net") as string;
+            short _port = (short)Function.IntSafeConvert(webconf.get("smtp.port"), 25);
+            string _username = webconf.get("smtp.username", string.Empty) as string;
+            string _pwd = webconf.get("smtp.password", string.Empty) as string;
+
+            if (string.IsNullOrEmpty(_username))
+                Smtp.Send(message, _server, _port);
+            else
+                Smtp.Send(message, _server, _port, GetDomain(message.From.Email), SmtpAuthentication.Any, _username, _pwd);
+        }
+
+        private static string GetDomain(string email)
+        {
+            if (string.IsNullOrEmpty(email)) return email;
+            int index = email.IndexOf('@');
+            return email.Substring(index + 1);
+        }
+        /// <summary>
+        /// Send an email to somebody
+        /// </summary>
+        /// <param name="strSubject">the subject</param>
+        /// <param name="strContent">the content which supports HTML</param>
+        /// <param name="strFrom">the email address from which this email will be delivered</param>
+        /// <param name="strToEmail">the receipient</param>
+        public static void SendEmail(string strSubject, string strContent, string strToEmail)
+        {
+            List<string> list = new List<string>(1);
+            list.Add(strToEmail);
+            SendEmail(strSubject, strContent, list);
+        }
+    }
+}
---
+++ email.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using OpenSmtp.Mail;
+using System.Collections;
+
+namespace Helper
+{
+    /// <summary>
+    /// email 的摘要说明
+    /// </summary>
+    public class EmailMan
+    {
+        Conf conf = Conf.CreateFromWebConfig();
+        public EmailMan() { }
+        /// <summary>
+        /// a simple way to send a single email using OpenSmtp.
+        /// the sender is configured in Web.config
+        /// </summary>
+        /// <param name="strTo">the recipicent</param>
+        /// <param name="strSubject">the subject</param>
+        /// <param name="strContent">the content</param>
+        /// <returns></returns>
+        public string SendEmail(string strTo, string strSubject, string strContent)
+        {
+            ArrayList al = new ArrayList(new EmailAddress[] { new EmailAddress(strTo) });
+            return SendEmail(conf.get("email") as string, al, strSubject, strContent);
+        }
+        /// <summary>
+        /// send one or more emails
+        /// </summary>
+        /// <param name="from">sender's email</param>
+        /// <param name="arTo">an array of EmailAddress objects to receive</param>
+        /// <param name="subject">the subject</param>
+        /// <param name="body">the body</param>
+        /// <returns>null on success, or an error description when failed</returns>
+        public string SendEmail(string from, ArrayList arTo, string subject, string body)
+        {
+            try
+            {
+                // no email format verification
+                SmtpConfig.VerifyAddresses = false;
+                // the message to send
+                MailMessage msg = new MailMessage();
+                msg.Charset = "gbk";
+                msg.From = new EmailAddress(from, Conf.CreateFromWebConfig().get("AppNameShort") as string);
+                msg.To = arTo;
+                msg.Subject = subject;
+                msg.Body = body;
+                // send it
+                new Smtp
+                    (conf.get("primarySmtpHost") as string,
+                    conf.get("emailAccountName") as string,
+                    conf.get("emailAccountPwd") as string,
+                    Helper.Function.IntSafeConvert(conf.get("primarySmtpPort")))
+                    .SendMail(msg);
+
+                return null;
+            }
+            catch (Exception e)
+            {
+                return e.Message + e.StackTrace;
+            }
+
+        }
+    }
+
+}
